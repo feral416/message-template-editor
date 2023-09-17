b2b0f7c82aca88ad094f3d19a74f1da5ef989bf2
Nick <73760287+feral416@users.noreply.github.com>
2023-09-17 12:12:42 +0300

    added flex

--- src/components/if_then_else.jsx
+++ src/components/if_then_else.jsx
@@ -3,9 +3,9 @@ import styles from '../styles/if_then_else.module.css';
 export default function IfThenElse() {
   return (
     <div className={styles.if_then_else_container}>
-      <div><span className={styles.row_name}>IF</span><button type="button" className={styles.delete_btn}>Delete</button></div> <input type="text"></input>
-      <div><span className={styles.row_name}>THEN</span></div>                     <input type="text"></input>
-      <div><span className={styles.row_name}>ELSE</span></div>                     <input type="text"></input>
+      <div><span className={styles.row_name}>IF</span><button type="button" className={styles.delete_btn}>Delete</button><input type="text"></input></div>
+      <div><span className={styles.row_name}>THEN</span><input type="text"></input></div>
+      <div><span className={styles.row_name}>ELSE</span><input type="text"></input></div>
     </div>
   );
 }
